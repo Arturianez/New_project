1d800ec36d5e5cb5534a8a6898cf0cdc258f1906
Arturianez <artur.zank@gmail.com>
2023-08-23 20:41:57 -0400

    NewAttempt

---
+++ script.js
@@ -0,0 +1,14 @@
+const array = ["one", 'two', 'three', 1,2,3]
+
+for (let i = 0; i < array.length; i++) {
+    if(array[i]===String(array[i])){
+        console.log(array[i])
+    } else {
+        console.log(array[i])
+    }
+}
+
+const a = true;
+if(!a){
+    console.log('false')
+}
